5b17048d46e3c5e07fb6f3cb792fff75a7084104
KreshsDesktop <ckresh99@gmail.com>
2022-03-29 18:50:07 -0400

    initial setting up

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            {/* Wrap Route elements in a Routes component */}
+            <Routes>
+              {/* Define routes using the Route component to render different page components at different paths */}
+              {/* Define a default route that will render the Home component */}
+              <Route path="/" element={<Home />} />
+              
+              <Route path="/profiles/:profileId" element={<Profile />} />
+              {/* Define a route that will take in variable data */}
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
